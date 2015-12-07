c23d274e67acb6157a9016971272256e41ddde24
Quanlong <kyan.ql.he@gmail.com>
2015-12-07 10:34:05 +0800

    [xcode] Fix broken link

--- Casks/xcode.rb
+++ Casks/xcode.rb
@@ -3,7 +3,7 @@ cask :v1 => 'xcode' do
   sha256 "68e4062c52be3e04c84539c1d6f4e816fec5edaf297a848ccf6e3508c77b99b6"
 
   # To download Xcode.dmg, see http://stackoverflow.com/a/10335943/622662
-  url 'http://10.60.1.18:8082/files/5b7927fb-7b55-4def-8540-3fc34e4227ae/Xcode_7.1.1.dmg'
+  url 'http://10.60.1.18:8082/files/f84c5cd2-d368-4bec-9520-b85b534ea169/Xcode_7.1.1.dmg'
   name 'Xcode'
   homepage 'https://developer.apple.com'
   license :freemium
